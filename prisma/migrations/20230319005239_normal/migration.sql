c1e09ccabbb0f644300080a5ff0941f351757752
student-ops <gogakuec@gmail.com>
2023-03-22 14:07:14 +0900

    delete migrations

--- prisma/migrations/20230319005239_normal/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "WebhookCommit" ADD COLUMN     "checked" BOOLEAN NOT NULL DEFAULT false;
--- prisma/migrations/20230322033342_normal/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `sugested` on the `Tasks` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Tasks" DROP COLUMN "sugested",
-ADD COLUMN     "suggested" BOOLEAN NOT NULL DEFAULT false;
