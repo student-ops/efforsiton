88704b0a248f4687d94f084b6e1153dd962be905
student-ops <gogakuec@gmail.com>
2023-04-19 20:45:35 +0900

    add some description

--- src/components/description.tsx
+++ src/components/description.tsx
@@ -3,6 +3,7 @@ const Description: React.FC = () => {
         <>
             <div className="">
                 <h1>description</h1>
+                <p>add some description</p>
             </div>
         </>
     )
