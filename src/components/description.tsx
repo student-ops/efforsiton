1bf626f94f7a273826154d9d46dd5071b4578c88
student-ops <gogakuec@gmail.com>
2023-04-24 22:45:00 +0900

    add svg and update description

--- src/components/description.tsx
+++ src/components/description.tsx
@@ -2,8 +2,12 @@ const Description: React.FC = () => {
     return (
         <>
             <div className="">
-                <h1>description</h1>
-                <p>add some description</p>
+                <p>Efforsitionは開発者向けAIタスク管理ツールです。</p>
+                <p>
+                    コードの更新から、実装された機能を推測しタスクの達成を判定します。
+                </p>
+                <b>処理のイメージ</b>
+                <img src="efforsition_descriptione.jpg" alt="service_image" />
             </div>
         </>
     )
